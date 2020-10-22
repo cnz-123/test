eaf91b1c8f6da2a3cdf6e3849e5887889dd9db35
cnz-123 <2027218979@qq.com>
2020-10-22 10:42:16 +0800

    hhh

---
+++ src/java/BreakpointDemo.java
@@ -0,0 +1,20 @@
+package java;
+
+public class BreakpointDemo {
+    public static void main(String[] arg){
+        int sum = 0;
+        int k=0;
+        System.out.println("Hello, Breakpoint!");
+        while (sum<50000){
+        k++;
+        for (int i=0;i<100;i++){
+             sum = sum + i;
+             System.out.println("i = "+i);
+             System.out.println("sum = "+sum);
+          }
+          System.out.println("k = "+ k);
+          System.out.println("sum = "+sum);
+
+        } 
+        }
+    }
